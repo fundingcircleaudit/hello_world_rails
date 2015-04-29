131d7f1580f397a21dd9d5b1a98cbef99765c492
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-04-29 16:04:22 +0100

    Update endpoint for demo
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,12 @@ before "deploy" do
 end
 
 after "deploy" do
-  Fc::Deploy::DeployProbe.for(application, "http://localhost:3000/deploys").send_event!
+  # Fc::Deploy::DeployProbe.for(application, "http://localhost:3000/deploys").send_event!
+  Fc::Deploy::DeployProbe.for(application, "http://shipment-tracker.herokuapp.com/deploys").send_event!
 end
 
 task :deploy do
+  puts "\tdeploying"
   # system "cf push #{application}"
+  puts "\tdeployed!"
 end
