8e9bad9e9c100041f06e81c4274fc40f4c224ccc
Dennis Ideler <dennis.ideler@fundingcircle.com>
2015-04-30 12:55:42 +0100

    Ensure that a specific version can be deployed to Heroku
    Signed-off-by: Theo Cushion <tcushion@pivotal.io>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,20 @@
+require "fc/deploy/deploy_probe"
+require "capistrano/recipes/deploy/scm"
+
 set :application, "hello_world_rails"
+set :repository, "git@github.com:FundingCircle/hello_world_rails.git"
+set :shipment_tracker, "http://shipment-tracker.herokuapp.com/deploys"
 
-require "fc-deploy"
-
-before "deploy" do
-end
-
-after "deploy" do
-  # Fc::Deploy::DeployProbe.for(application, "http://localhost:3000/deploys").send_event!
-  Fc::Deploy::DeployProbe.for(application, "http://shipment-tracker.herokuapp.com/deploys").send_event!
-end
+# Reconstructing interface usually setup by capistrano/recipes/deploy
+set(:source) { Capistrano::Deploy::SCM.new(:git, self) }
+set(:revision) { source.head }
+set(:real_revision) { source.local.query_revision(revision) { |cmd| `#{cmd}` } }
 
 task :deploy do
-  puts "\tdeploying"
-  system "cf push #{application}"
-  puts "\tdeployed!"
+  system "git push --force heroku #{real_revision}:master"
+end
+
+# Deploy hook to record what got deployed
+after "deploy" do
+  Fc::Deploy::DeployProbe.for(application, shipment_tracker).send_event!
 end
