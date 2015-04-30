3769fef148668c30208ee9757c90caa80dfbcb0c
Dennis Ideler <dennis.ideler@fundingcircle.com>
2015-04-30 14:50:27 +0100

    Add support for TAG environment variable
    Signed-off-by: Theo Cushion <tcushion@pivotal.io>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 require "fc/deploy/deploy_probe"
 require "capistrano/recipes/deploy/scm"
 
+# Standard FC deploy.rb settings
 set :application, "hello_world_rails"
 set :repository, "git@github.com:FundingCircle/hello_world_rails.git"
 set :shipment_tracker, "http://shipment-tracker.herokuapp.com/deploys"
+set :branch, ENV['TAG'] || "origin/#{ENV['BRANCH'] || 'production'}"
 
 # Reconstructing interface usually setup by capistrano/recipes/deploy
 set(:source) { Capistrano::Deploy::SCM.new(:git, self) }
